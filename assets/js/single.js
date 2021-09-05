223de536a7a9426092b86309a1c1cb5d2f2289b2
ajia <ajialeeholiday@gmail.com>
2021-09-05 15:30:57 -0600

    finish single and list issues page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,25 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // dipsplay repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repoi was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+
+    
+    
+};
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -14,7 +34,8 @@ var getRepoIssues = function (repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
    
@@ -68,4 +89,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEL);
 };
 
-getRepoIssues("facebook/react");
+getRepoName()
